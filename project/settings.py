f25ec69e72584f09562616f5c2876cd1153417c3
alaa albasha <alaaalbassha0@gmail.com>
2025-08-23 16:50:19 +0300

    edit cache

--- project/settings.py
+++ project/settings.py
@@ -39,13 +39,11 @@ INSTALLED_APPS = [
 ]
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake",
     }
 }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
